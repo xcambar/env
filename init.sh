a97d9906a8ad5599dc6a1fdde63070f239e27d62
Xavier Cambar <xavier.cambar@lecoffre.net>
2012-11-10 19:52:18 +0100

    Add issues, moves the rvm config to init.sh, install jshint by default

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 source ~/.dotfiles/vms/nvm/nvm.sh
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 alias n=node
+alias r=ruby

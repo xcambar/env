5c301e2301e59b72f52437a14737b54c587d70a0
Xavier Cambar <xavier.cambar@lecoffre.net>
2012-11-11 12:46:03 +0100

    Adds a function c to go directly into a subfolder of $codePath

--- init.sh
+++ init.sh
@@ -1,6 +1,14 @@
+#!/usr/bin/env zsh
 source ~/.dotfiles/vms/nvm/nvm.sh
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 alias n=node
 alias r=ruby
+
+#Defines the helper to go directly to a subdirectory of $codePath
+local codePath="$HOME/code/"
+function c () {
+  cd $codePath$1
+}
+compdef '_files -W "$codePath/" -/' c

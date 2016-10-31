6a9c3e9b67eb50ccc68a5d288a42bcf91abafc5d
Xavier Cambar <xcambar@gmail.com>
2016-10-31 17:17:50 +0100

    Better base16

--- zsh/_base16.zsh
+++ zsh/_base16.zsh
@@ -2,26 +2,8 @@
 # Base16 Shell
 #
 
-function b16 () {
-  local theme="$1";
-  local mode=".$2";
-  local def="$BASE16_LOCATION/scripts/base16-$theme$mode.sh"
-  local def_simple="$BASE16_LOCATION/scripts/base16-$theme.sh"
-  if [[ -s $def ]]; then
-    . "$def"
-    export BASE16_SCHEME="$theme"
-    export BASE16_MODE="$mode"
-  elif [[ -s $def_simple ]]; then
-    . "$def_simple"
-    export BASE16_SCHEME="$theme"
-    export BASE16_MODE=""
-  fi
-}
+: ${BASE16_SHELL:="$HOME/.config/base16-shell"}
+: ${BASE16_MODE:="dark"}
 
-function b16t () {
-  b16 "$1" "$BASE16_MODE"
-}
-
-function b16m () {
-  b16 "$BASE16_SCHEME" "$1"
-}
+# Add helpers
+[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
